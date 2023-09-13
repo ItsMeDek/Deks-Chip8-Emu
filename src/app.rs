7777a3976478be420530d849453f468121f9f5ed
Dek Aister <Dek Aister>
2023-09-13 17:37:14 +0200

    Add some opcodes and fix program counter

--- src/app.rs
+++ src/app.rs
@@ -48,13 +48,29 @@ impl Emulator {
         }
 
         let opcode = self.fetch_opcode();
-        match opcode {
+        match opcode & 0xF000 {
+            0x0000 => {
+                match opcode & 0x00FF {
+                    0xE0 => {
+                        self.clear_screen();
+                    },
+                    _ => {
+                        panic!("Unknown opcode 0x{:x}!", opcode)
+                    }
+                }
+            },
+            0x6000 => {
+                let register_index = ((opcode & 0x0F00) >> 8) as u8;
+                let value = (opcode & 0x00FF) as u8;
+
+                self.set_register(register_index, value);
+            },
             _ => {
                 panic!("Unknown opcode 0x{:x}!", opcode);
             }
         }
 
-        self.pc += 1;
+        self.pc += 2;
     }
 
     fn fetch_opcode(&self) -> u16 {
@@ -68,6 +84,20 @@ impl Emulator {
     #[doc = "Jump to a specific place in memory"]
     fn jump(&mut self, location: u16) {
         self.pc = location as usize;
+    }
+
+    #[doc = "Set a Vx register to the specified value"]
+    fn set_register(&mut self, register_index: u8, value: u8) {
+        self.vx[register_index as usize] = value;
+    }
+
+    #[doc = "Override the entire vram with 0's"]
+    fn clear_screen(&mut self) {
+        for row in self.video_memory {
+            for mut column in row {
+                column = 0;
+            }
+        }
     }
 }
 
